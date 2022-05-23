a6b006c3c9b672f533cc41ffb6d163e7b975804f
Dax2976 <98029197+Dax2976@users.noreply.github.com>
2022-05-23 13:16:36 +0200

    end

---
+++ snack_1/index.php
@@ -0,0 +1,74 @@
+<!-- Partendo da questo array: https://www.codepile.net/pile/Po60bjgQ
+Ad ogni refresh della pagina visualizzare una pubblicità a schermo, 
+tenendo conto che possono essere sorteggiate solo quelle is_active true. -->
+
+<?php
+
+$ads = [
+    [
+        'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
+        'link' => 'https://www.google.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
+        'link' => 'https://www.facebook.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
+        'link' => 'https://codepen.io',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
+        'link' => 'https://laravel.com',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
+        'link' => 'https://www.php.net',
+        'is_active' => true,
+    ],
+
+];
+
+$active_ads=[];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Snack_1</title>
+</head>
+<body>
+    <?php foreach($ads as $ad){
+        if($ad['is_active']){
+            array_push($active_ads,$ad);
+        }
+        ?>
+    <?php } ?>  
+        <div class="container">
+        <h1 class="text-uppercase">pubblicità</h1>
+            <div class="row">
+                <?php 
+                    echo '<img src="';
+                    echo $active_ads[rand(0,count($active_ads)-1)]['image_path'];
+                    echo '" alt="">';
+                ?>
+            </div>
+        </div>
+   
+
+
+
+
+
+</body>
+</html>
